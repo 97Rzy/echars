f1dd9b7a51b72d01db8794138b96dda270ed3f8b
zy <527104183@qq.com>
2021-01-18 14:15:37 +0800

    添加注释

--- components/echarts/InitEcharts.vue
+++ components/echarts/InitEcharts.vue
@@ -13,6 +13,7 @@ export default {
       let eCharts = this.$echarts.init(document.getElementById('myEchars'))
       // 指定图表的配置项和数据
       var option = {
+        // echarts的标题
         title: {
           text: 'ECharts 入门示例'
         },
@@ -20,10 +21,12 @@ export default {
         legend: {
           data: ['销量']
         },
+        // x轴的名称
         xAxis: {
           data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
         },
         yAxis: {},
+        // 设置echarts的类型,以及显示的数据
         series: [{
           name: '销量',
           type: 'bar',
