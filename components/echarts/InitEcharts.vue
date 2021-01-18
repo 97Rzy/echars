9d12ccccb6371233c90a76827a6ef6b31ac5767e
zy <527104183@qq.com>
2021-01-18 11:42:17 +0800

    初始化Echarts

---
+++ components/echarts/InitEcharts.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="myEchars"></div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    echartsInit () {
+      // 基于准备好的dom，初始化echarts实例
+      let eCharts = this.$echarts.init(document.getElementById('myEchars'))
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      eCharts.setOption(option);
+    }
+  },
+  mounted () {
+    this.echartsInit()
+  },
+}
+</script>
+<style lang="less">
+#myEchars {
+  width: 600px;
+  height: 300px;
+  background-color: #eee;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <h1>echars学习</h1>
-    <div id="main" style="width: 600px;height:400px;"></div>
+    <div class="echarts-box">
+      <h1>echars学习</h1>
+      <init-echarts></init-echarts>
+    </div>
   </div>
 </template>
 
 <script>
+import InitEcharts from '~/components/echarts/InitEcharts'
 export default {
+  name: 'index',
+  data () {
+    return {
 
+    }
+  },
+  components: {
+    InitEcharts
+  }
 }
 </script>
 <style lang="less" scoped>
+.echarts-box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  h1 {
+    margin-bottom: 50px;
+  }
+}
 </style>
